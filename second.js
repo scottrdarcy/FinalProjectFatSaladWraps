a2166dc283dc61045bd148ebe9d63c999ca63405
Scott D <46800124+scottrdarcy@users.noreply.github.com>
2019-03-06 10:48:54 -0500

    Add files via upload
    I imported these files on 3/6.  I have been working off GitHub for the past month

---
+++ second.js
@@ -0,0 +1,24 @@
+
+var lettuce;
+var xLettuce;
+var Onion;
+var xOnion;
+var cheese;
+var xcheese;
+var Tomato;
+var xTomato; 
+var Carrot;
+var xCarrot;
+var FriedOnions; 
+var ThaiGing;
+var French;
+var BC;
+
+function SaladSelector(){
+    If 
+
+}
+
+var SaladCombos = ["lettuce","xlettuce","Onion","xOnion","cheese", "xcheese","Tomato","xTomato","Carrot","xCarrot"];
+
+
---
+++ special.js
@@ -0,0 +1,141 @@
+
+
+
+
+
+
+
+
+function mainMenuFunction(name) { 
+  prompt ("Would you like to order a " +  name + "?");
+}
+
+function FruityBootyFunction(Name) { 
+  prompt ("You really want the " +  Name + "?" );
+}
+
+
+/*
+
+function mainMenuFunction() { 
+   prompt ("Would you like to order a Salad?");
+}
+
+var Button=document.getElementById("mainSelection");
+var Button=document.getElementById("fruitybooty");
+
+
+
+/*
+prompt ("Would You like to Order a Salad?");
+  prompt ("You want the FruityBooty?");
+
+document.getElementById("mainselection").onclick = displayDate;
+
+
+
+
+
+<button onclick="myFunction()">Try it</button>
+
+<p id="demo"></p>
+
+<script>
+function myFunction() {
+  var txt;
+  var person = prompt("Please enter your name:", "Harry Potter");
+  if (person == null || person == "") {
+    txt = "User cancelled the prompt.";
+  } else {
+    txt = "Hello " + person + "! How are you today?";
+  }
+  document.getElementById("demo").innerHTML = txt;
+}
+</script>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var BGN;
+var TP;
+var CBC;
+var FB;
+var CYO;
+
+
+funtion MainMenuSaladOrder();
+
+
+
+
+var Lett;
+var XLett;
+var Onion;
+var XOnion;
+var Cheese;
+var XCheese;
+var tomato;
+var Xtom;
+var Carrot;
+var XCarrot;
+var FriedOnion;
+var ThaiGing;
+var French;
+var BlueCheese;
+
+funtion ChangePic();
+
+
+
+funtion CalcCreatYourOwn();
+
+
+
+funtion CalculateSubtotal();
+
+
+function CalculateTax();
+
+function CalculateTotal();
+
+
+
+
+
+
+
+
+
+
+
+
+function getValue() {
+    var retVal = prompt("Enter your name : ", "your name here");
+    document.write("You have entered : " + retVal);
+ }
+
+ $(document).ready(function ()
+{
+    function click()
+    {
+        $('#dialog').dialog({
+            autoOpen: false,
+            width: 250,
+            height: 180,
+            modal : true
+        });
+    }
+});
+
+*/
+
